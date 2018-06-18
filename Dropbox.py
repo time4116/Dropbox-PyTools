41d089381b05665e675a1bd664ab39f80ec56170
time4116 <31143754+time4116@users.noreply.github.com>
2018-06-18 16:34:03 -0700

    Updated Workflow
    Added logic to replace invalid characters from file paths and removed redundant global variables ("allFiles"). Fixed issues where script would error out because listUserFolder() has_more value was false and therefore listUserFolderContinue() did not return anything.

--- Dropbox.py
+++ Dropbox.py
@@ -1,4 +1,4 @@
-import requests, os, logging
+import requests, os, logging, csv
 
 # Make sure to set the below vars before running the script!! Also make sure to set limit in getAllUSers() the max and default is 1000.
 # See line 94 (downloadUserFolder()) to specify any top level folders to exclude, or just comment it out.
@@ -7,8 +7,8 @@ import requests, os, logging
 # https://www.dropbox.com/developers/documentation/http/overview
 # https://www.dropboxforum.com/t5/Developer-Community-API-Support/ct-p/101000041
 
-root = 'E:\\'
-domain = '@yourcompany.com' # Removes the domain from the email address (Used to create the folder)
+root = 'C:\\temp'
+domain = '@yourcompany.com'  # Removes the domain from the email address (Used to create the folder)
 logName = 'dropbox_download.log'
 logFile = root + logName
 
@@ -18,6 +18,7 @@ token = 'Your Key'
 # Logging stuff
 logging.basicConfig(filename=logFile, level=logging.DEBUG,
                     format='%(asctime)s:%(levelname)s:%(message)s')
+
 
 # Returns a dictionary of user attributes
 def getAllUsers():
@@ -32,13 +33,13 @@ def getAllUsers():
     json = response.json()
     return json['members']
 
+
 # Lists the contents of a users folders. If the result's listFolderResult.has_more field is true, call list_folder/continue (or listUserFolderContinue) with the returned ListFolderResult.cursor to retrieve more entries.
 def listUserFolder(dbmid):
-    global allFiles
 
     headers = {
         'Authorization': 'Bearer {}'.format(token),
-        'Content-Type': 'application/json; charset=utf-8', #Changed this char set
+        'Content-Type': 'application/json; charset=utf-8',  # Changed this char set
         'Dropbox-API-Select-User': dbmid
     }
 
@@ -46,14 +47,17 @@ def listUserFolder(dbmid):
 
     response = requests.post('https://api.dropboxapi.com/2/files/list_folder', headers=headers, data=data)
     json = response.json()
-    allFiles += json['entries']
     return json
 
 # Paginate and return a complete list of file\folder contents for a users folder.
 def listUserFolderContinue(dbmid):
-    global allFiles
+    allfiles = []
 
     json = listUserFolder(dbmid)
+    allfiles += json['entries']
+
+    if json['has_more'] == False:
+        return allfiles
 
     while json['has_more'] == True:
 
@@ -63,22 +67,18 @@ def listUserFolderContinue(dbmid):
 
         headers = {
             'Authorization': 'Bearer {}'.format(token),
-            'Content-Type': 'application/json; charset=utf-8', #Changed this char set
+            'Content-Type': 'application/json; charset=utf-8',  # Changed this char set
             'Dropbox-API-Select-User': dbmid
         }
 
         response = requests.post('https://api.dropboxapi.com/2/files/list_folder/continue', headers=headers, data=data)
         json = response.json()
-        allFiles += json['entries']
-        return allFiles
+        allfiles += json['entries']
+        return allfiles
 
 # Downloads all of the contents of a users folder and retains the folder hierarchy (Except if the full path exceeds 255 characters. Windows limitation)
 # Teams folders are also excluded from this function. Chunk size allows for better performance when downloading large files.
 def downloadUserFolder(dbmid, username, root):
-    global allFiles
-
-    allFiles = []
-
     data = listUserFolderContinue(dbmid)
 
     for content in data:
@@ -91,20 +91,37 @@ def downloadUserFolder(dbmid, username, root):
             pass
             # Folders are just pointers and can not actually be downloaded. The full path is needed.
 
-        # elif '/_Teams1' in fpath or '/_Teams2' in fpath or '/_Teams3' in fpath:
-            # pass
-            # # Exclude all teams folders from being downloaded.
+        #elif '/_Teams1' in fpath or '/_Teams2' in fpath or '/_Teams3' in fpath:
+            #pass
+            # # Exclude folders from being downloaded.
 
         else:
             size = content['size']
             fullpath = content['path_display']
             orgpath = content['id']
             dest = root + username + fullpath
-            dest = dest.replace("/","\\")
+            dest = dest.replace("/", "\\")
+
+            # Renames invalid character ':' to -BADCHAR-. Place exceptions in this if statement.
+            # Certain characters are not permitted depending on the operating system.
+            dirsplit = dest.split('\\')
+            dirlen = len(dirsplit)
+            for i in range(1, dirlen):  # Set to 1 to skip the ':' in the root path C:\\ etc...
+                if ':' in dirsplit[i]:
+                    badchar = dirsplit[i].replace(':', '-BADCHAR-')
+                    dest = dest.replace(dirsplit[i], badchar)
+
+                    dest = dest + '\\' + filename
+                    logging.debug("____ {} File name had a bad char in it and was renamed. '-BADCHAR-'".format(
+                        dest.encode("utf-8")))
+
+                else:
+                    pass
 
             if len(dest) >= 255:
-                dest = root+username+'\\255\\'+filename
-                logging.debug("____ {} Exceeded Windows 255 char limit and was renamed a moved to 255 folder".format(dest.encode("utf-8")))
+                dest = root + username + '\\255\\' + filename
+                logging.debug("____ {} Exceeded Windows 255 char limit and was renamed a moved to 255 folder".format(
+                    dest.encode("utf-8")))
                 # Write to log file the orginal dropbox path of this folder as it may exceed the windows 255 char limit
             else:
                 pass
@@ -115,12 +132,12 @@ def downloadUserFolder(dbmid, username, root):
                 os.makedirs(dir)
             else:
                 pass
-                #print("{} already exists!".format(dir))
+                # print("{} already exists!".format(dir))
 
             if size >= 524288000 and os.path.isfile(dest) != True:
 
                 logging.debug("DropBox: {} (Large File) Downloading...".format(fullpath.encode("utf-8")))
-                DropboxAPIArg = '{"path": "'+orgpath+'"}'
+                DropboxAPIArg = '{"path": "' + orgpath + '"}'
 
                 headers = {
                     'Authorization': 'Bearer {}'.format(token),
@@ -128,11 +145,12 @@ def downloadUserFolder(dbmid, username, root):
                     'Dropbox-API-Select-User': dbmid
                 }
 
-                response = requests.post('https://content.dropboxapi.com/2/files/download', headers=headers, stream=True)
+                response = requests.post('https://content.dropboxapi.com/2/files/download', headers=headers,
+                                         stream=True)
 
                 try:
                     handle = open(dest, "wb")
-                    for chunk in response.iter_content(chunk_size=10485760): # changed from 512 to 10485760 (10 MB)
+                    for chunk in response.iter_content(chunk_size=10485760):  # changed from 512 to 10485760 (10 MB)
                         if chunk:  # filter out keep-alive new chunks
                             handle.write(chunk)
 
@@ -169,6 +187,7 @@ def downloadUserFolder(dbmid, username, root):
 
                 logging.debug("{} File has already been downloaded...".format(dest.encode("utf-8")))
 
+
 # Performs the download function for each user returned via getAllUsers(). Remember to set the limit value in that function
 # as it can be set to a max of 1000 (Which is the default) if there are ever more users than that
 # a new function similar to listFolderContinue() will need to be created to paginate the results of getAllUsers().
@@ -182,12 +201,12 @@ def MassUserCopy():
         status = str(user['profile']['status'])
         dbmid = user['profile']['team_member_id']
         email = user['profile']['email']
-        username = email.replace(domain,'')
-       
+        username = email.replace(domain, '')
+
         if 'active' in status:
-                logging.debug("Starting download function for the following dbmid: {}".format(dbmid))
-                logging.debug("Starting download function for the following user: {}".format(username))
-                downloadUserFolder(dbmid, username, root)
+            logging.debug("Starting download function for the following dbmid: {}".format(dbmid))
+            logging.debug("Starting download function for the following user: {}".format(username))
+            downloadUserFolder(dbmid, username, root)
 
     logging.debug("Script has completed. (See log for errors)")
 
@@ -198,20 +217,20 @@ def MassUserCopyFromCSV(csv_path):
 
     with open(csv_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
-        next(csv_reader) # Skip first row (Headers)
+        next(csv_reader)  # Skip first row (Headers)
         content = csv_reader
 
         for line in content:
 
             # Make sure to adjust if needed!!
-            status = str(line[4]) # Status header is in postion 4 of my CSV
-            dbmid = line[0] # dbmid header is in postion 0 of my CSV
-            email = line[2] # email header is in postion 2 of my CSV
-            username = email.replace(domain,'')
-            
+            status = str(line[4])  # Status header is in postion 4 of my CSV
+            dbmid = line[0]  # dbmid header is in postion 0 of my CSV
+            email = line[2]  # email header is in postion 2 of my CSV
+            username = email.replace(domain, '')
+
             if 'active' in status:
-                    logging.debug("Starting download function for the following dbmid: {}".format(dbmid))
-                    logging.debug("Starting download function for the following user: {}".format(username))
-                    downloadUserFolder(dbmid, username, root)
+                logging.debug("Starting download function for the following dbmid: {}".format(dbmid))
+                logging.debug("Starting download function for the following user: {}".format(username))
+                downloadUserFolder(dbmid, username, root)
 
     logging.debug("Script has completed. (See log for errors)")
